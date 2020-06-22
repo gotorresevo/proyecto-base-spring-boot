ebcc69e392c4931399d078b533bf7ae590c95161
gotorresevo <gotorres_ext@bankinter.com>
2020-06-22 15:39:13 +0200

    Limpiando el subscriptor del evento

--- src/main/java/com/evobank/shopping/submodules/products/application/bpm/domainEventSubscriber/UpdateStockOnProductAggregatedToCart.java
+++ src/main/java/com/evobank/shopping/submodules/products/application/bpm/domainEventSubscriber/UpdateStockOnProductAggregatedToCart.java
@@ -6,15 +6,9 @@ import com.evobank.shopping.submodules.shared.carts.domain.events.ProductAggrega
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.camunda.bpm.engine.ProcessEngine;
-import org.camunda.bpm.engine.RepositoryService;
-import org.camunda.bpm.engine.RuntimeService;
-import org.camunda.bpm.engine.repository.ProcessDefinition;
-import org.camunda.bpm.engine.runtime.ProcessInstance;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.EventListener;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 @DomainEventSubscriber({ProductAggregateToCartDomainEvent.class})
@@ -22,22 +16,14 @@ import java.util.Map;
 @Slf4j
 public final class UpdateStockOnProductAggregatedToCart {
 
-    @Autowired
-    private ProcessEngine processEngine;
+    private final ProcessEngine processEngine;
 
     @EventListener
     public void on(ProductAggregateToCartDomainEvent event) {
-        /**
-         * Se consulta la definicion de procesos, que no son mas que los archivos *.bpmn y sus versiones.
-         */
-        List<ProcessDefinition> processDefinitions = processEngine.getRepositoryService().createProcessDefinitionQuery()
-                .orderByProcessDefinitionVersion()
-                .asc()
-                .list();
         Map<String, Object> variables = new HashMap<>();
         variables.put("idProduct", event.getIdProduct());
         variables.put("quantity", event.getQuantity());
-        ProcessInstance processInstance = processEngine.getRuntimeService()
+        processEngine.getRuntimeService()
                 .startProcessInstanceByKey("process-stock-products", variables);
     }
 }
