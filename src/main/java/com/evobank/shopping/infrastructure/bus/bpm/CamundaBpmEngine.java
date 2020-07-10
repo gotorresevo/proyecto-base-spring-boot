462d560afe7d6f42c3b11bd4f08470c9d0f87c08
gotorresevo <gotorres_ext@bankinter.com>
2020-07-10 19:24:50 +0200

    Ajustando detalles

--- src/main/java/com/evobank/shopping/infrastructure/bus/bpm/CamundaBpmEngine.java
+++ src/main/java/com/evobank/shopping/infrastructure/bus/bpm/CamundaBpmEngine.java
@@ -3,10 +3,7 @@ package com.evobank.shopping.infrastructure.bus.bpm;
 import com.evobank.architecture.businessprocess.bpm.IBpmEngine;
 import com.evobank.architecture.infrastructure.InjectDependency;
 import lombok.AllArgsConstructor;
-import org.camunda.bpm.engine.HistoryService;
 import org.camunda.bpm.engine.ProcessEngine;
-import org.camunda.bpm.engine.RuntimeService;
-import org.camunda.bpm.engine.TaskService;
 import org.camunda.bpm.engine.history.HistoricDecisionInstance;
 import org.camunda.bpm.engine.runtime.Execution;
 import org.camunda.bpm.engine.runtime.ProcessInstance;
@@ -22,9 +19,6 @@ import java.util.stream.Stream;
 public final class CamundaBpmEngine implements IBpmEngine {
 
     private final ProcessEngine processEngine;
-    private final TaskService taskService;
-    private final RuntimeService runtimeService;
-    private final HistoryService historyService;
 
     @Override
     public String startProcessInstance(String idProcess, String key, Map<String, Object> variables) {
@@ -40,22 +34,22 @@ public final class CamundaBpmEngine implements IBpmEngine {
 
     @Override
     public void completeTask(String idProcess, String key, String userName) {
-        Task task = taskService.createTaskQuery()
+        Task task = processEngine.getTaskService().createTaskQuery()
                 .processDefinitionKey(idProcess)
                 .orderByDueDate().asc()
                 .processInstanceBusinessKey(key).list().get(0);
-        taskService.claim(task.getId(), userName);
-        taskService.complete(task.getId());
+        processEngine.getTaskService().claim(task.getId(), userName);
+        processEngine.getTaskService().complete(task.getId());
     }
 
     @Override
     public void sendSignal(String idProcess, String idReceiveTask, String value) {
-        ProcessInstance processInstance = runtimeService.createProcessInstanceQuery()
+        ProcessInstance processInstance = processEngine.getRuntimeService().createProcessInstanceQuery()
                 .processDefinitionKey(idProcess)
                 .processInstanceBusinessKey(value).singleResult();
-        Execution execution = runtimeService.createExecutionQuery()
+        Execution execution = processEngine.getRuntimeService().createExecutionQuery()
                 .processInstanceId(processInstance.getId()).activityId(idReceiveTask).singleResult();
-        runtimeService.signal(execution.getId());
+        processEngine.getRuntimeService().signal(execution.getId());
     }
 
     @Override
@@ -66,7 +60,7 @@ public final class CamundaBpmEngine implements IBpmEngine {
 
     @Override
     public Stream<Object> getOutputsBusinessRule(String idInstanceProcess, String idDecisionDefinition) {
-        List<HistoricDecisionInstance> historicDecisions = historyService.createHistoricDecisionInstanceQuery()
+        List<HistoricDecisionInstance> historicDecisions = processEngine.getHistoryService().createHistoricDecisionInstanceQuery()
                 .processInstanceId(idInstanceProcess)
                 .decisionDefinitionKey("DecisionTable")
                 .includeInputs()
